3c069a305e57b92db121c353a77f4ca2c9bbf270
Yafira <yafira.martinez@lc.cuny.edu>
2022-12-21 23:10:01 -0500

    getToday func for a more readable date format

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -46,3 +46,33 @@ const getPageMetaData = (post) => {
 		slug: post.properties.Slug.rich_text[0].plain_text,
 	}
 }
+
+function getToday(datestring) {
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	]
+
+	let date = new Date()
+
+	if (datestring) {
+		date = new Date(datestring)
+	}
+
+	const day = date.getDate()
+	const month = months[date.getMonth()]
+	const year = date.getFullYear()
+	let today = `${month} ${day}, ${year}`
+
+	return today
+}
