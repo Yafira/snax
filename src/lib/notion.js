84fb928e3b1667942e581ea5aef5966bab4779f3
Yafira <yafira.martinez@lc.cuny.edu>
2022-12-21 22:44:04 -0500

    created function for published posts

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -3,3 +3,27 @@ const { Client } = require('@notionhq/client')
 const notion = new Client({
 	auth: process.env.NOTION_TOKEN,
 })
+
+export const getAllPublished = async () => {
+	const posts = await notion.databases.query({
+		database_id: process.env.DATABASE_ID,
+		filter: {
+			property: 'Published',
+			checkbox: {
+				equals: true,
+			},
+		},
+		sorts: [
+			{
+				property: 'Date',
+				direction: 'descending',
+			},
+		],
+	})
+
+	const allPosts = posts.results
+
+	return allPosts.map((post) => {
+		return getPageMetaData(post)
+	})
+}
