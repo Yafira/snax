ff9e550b3b4290d27a5710b312c4f17ef9201764
Yafira <yafira.martinez@lc.cuny.edu>
2023-01-30 19:52:21 -0500

    updated footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import styles from '../styles/Footer.module.css'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
 	return (
 		<div className={styles.Footer}>
 			<h3>
-				Built and designed <FontAwesomeIcon className='icon' icon={faHeart} />{' '}
-				by Yafira Martinez
+				© 2023 Built and designed{' '}
+				<FontAwesomeIcon icon={faHeart} style={{ color: '#B2A4D4' }} /> by
+				Yafira Martinez
 			</h3>
 		</div>
 	)
