077afcce46efb2b08391b894b64d2767acbab0bc
yafira <yafira.martinez@lc.cuny.edu>
2023-04-05 21:07:53 -0400

    made additional changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import Logo from '../assets/snax-logo.png'
+import Hbg from '../assets/header.jpg'
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
+import { bgWrap } from '../styles/Header.module.css'
 
 export default function Header() {
 	return (
 		<div className={styles.Header}>
-			<Image className={styles.logo} src={Logo} alt='logo' />
-			<h1>Snax</h1>
-			<p>Like a Pokedex but for vegan-friendly snacks and other finds</p>
-			<h5>All snacks are consumed and reviewed by Yafira Martinez</h5>
+			<div className={bgWrap}>
+				<Image className={styles.hbg} src={Hbg} alt='header background image' />
+			</div>
+
+			<div className={styles.snaxBox}>
+				<Image className={styles.logo} src={Logo} alt='logo' />
+				<h1>Snax</h1>
+				<p>Like a Pokedex but for vegan-friendly snacks and other finds</p>
+				<h5>All snacks are consumed and reviewed by Yafira</h5>
+			</div>
 		</div>
 	)
 }
