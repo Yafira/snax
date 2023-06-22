428ed4ab9e0976f1c82a60df51cbdc2b8c2fe609
yafira <yafira.martinez@lc.cuny.edu>
2023-06-22 14:28:31 -0400

    updated favicon pkg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,7 +22,8 @@ export default function Document() {
 					href='/favicon-16x16.png'
 				/>
 				<link rel='manifest' href='/site.webmanifest' />
-				<meta name='msapplication-TileColor' content='#00aba9' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#000000' />
+				<meta name='msapplication-TileColor' content='#603cba' />
 				<meta name='theme-color' content='#ffffff' />
 			</Head>
 			<body>
