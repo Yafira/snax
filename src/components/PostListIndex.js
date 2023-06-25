784663b6e0afb700502b9ffdd87176466fb79f18
yafira <yafira.martinez@lc.cuny.edu>
2023-06-25 00:38:55 -0400

    implemented responsive masonry

--- src/components/PostListIndex.js
+++ src/components/PostListIndex.js
@@ -1,3 +1,4 @@
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import styles from '../styles/Home.module.css'
 import PostItem from './PostItem'
 
@@ -6,16 +7,22 @@ export default function PostListTags({ posts }) {
 
 	return (
 		<main className={`${styles.columns} ${styles.main}`}>
-			{posts?.map((post, index) => (
-				<PostItem
-					key={index}
-					imgURL={post.image}
-					title={post.title}
-					rating={post.rating}
-					description={post.description}
-					tags={post.tags}
-				/>
-			))}
+			<ResponsiveMasonry
+				columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1000: 4, 1200: 5 }}
+			>
+				<Masonry gutter={10}>
+					{posts?.map((post, index) => (
+						<PostItem
+							key={index}
+							imgURL={post.image}
+							title={post.title}
+							rating={post.rating}
+							description={post.description}
+							tags={post.tags}
+						/>
+					))}
+				</Masonry>
+			</ResponsiveMasonry>
 		</main>
 	)
 }
