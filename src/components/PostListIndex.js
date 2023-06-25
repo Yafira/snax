1e06649ce521dd91df24c199228983cda0345d73
yafira <yafira.martinez@lc.cuny.edu>
2023-06-25 00:41:27 -0400

    fixed problem in main className

--- src/components/PostListIndex.js
+++ src/components/PostListIndex.js
@@ -6,7 +6,7 @@ export default function PostListTags({ posts }) {
 	if (!posts) return <h1>No snacks available.</h1>
 
 	return (
-		<main className={`${styles.columns} ${styles.main}`}>
+		<main className={styles.main}>
 			<ResponsiveMasonry
 				columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1000: 4, 1200: 5 }}
 			>
